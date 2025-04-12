68ae1ade94f291c55cfb220c577f953b4f7749a3
kirias1996 <56193589+kirias1996@users.noreply.github.com>
2025-04-13 08:58:59 +0900

    日報登録機能の実装 (#8)

---
+++ src/main/java/com/example/study/dto/ReportRequestDto.java
@@ -0,0 +1,75 @@
+package com.example.study.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+public class ReportRequestDto {
+
+	private String title;
+
+	private String content;
+	
+	private LocalDate learningDate;
+	
+	@Min(0)
+	@Max(23)
+	private int learningHours;
+	
+	private int learningMinutes;
+	
+	public ReportRequestDto() {
+		
+	}
+	
+	/*カスタムバリデーション*/
+	@AssertTrue(message = "分は0,15,30,45のいずれかを選択してください。")
+	public boolean isValidMinutes() {
+		return List.of(0,15,30,45).contains(learningMinutes);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDate getLearningDate() {
+		return learningDate;
+	}
+
+	public void setLearningDate(LocalDate learningDate) {
+		this.learningDate = learningDate;
+	}
+
+	public int getLearningHours() {
+		return learningHours;
+	}
+
+	public void setLearningHours(int learningHours) {
+		this.learningHours = learningHours;
+	}
+
+	public int getLearningMinutes() {
+		return learningMinutes;
+	}
+
+	public void setLearningMinutes(int learningMinutes) {
+		this.learningMinutes = learningMinutes;
+	}
+
+
+}
