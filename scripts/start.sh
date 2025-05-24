e3014f85168047abef6553ec8b8afffb07a0a6ac
kirias1996 <arishou829@gmail.com>
2025-05-24 12:49:45 +0900

    アプリ起動用スクリプト作成

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "[start.sh] 開始：環境変数を読み込みます..."
+source .env
+
+echo "[start.sh] .env を削除します（セキュリティ対策）..."
+rm .env
+
+echo "[start.sh] Spring Boot アプリケーションを起動します..."
+java -jar build/libs/StudyLog.jar --spring.profiles.active=prod
