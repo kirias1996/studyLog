3dd78224f46532544d8c7122741325dbd9eb53d9
kirias1996 <arishou829@gmail.com>
2025-05-24 17:53:57 +0900

    環境変数を.envファイルに書き出す記述を削除

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -eu
 
-echo "[start.sh] 開始: 環境変数を .env に書き出します..."
-echo "DB_URL=${DB_URL}" > .env
-echo "DB_USERNAME=${DB_USERNAME}" >> .env
-echo "DB_PASSWORD=${DB_PASSWORD}" >> .env
-echo "CLOUDINARY_API_KEY=${CLOUDINARY_API_KEY}" >> .env
-echo "CLOUDINARY_API_SECRET=${CLOUDINARY_API_SECRET}" >> .env
-echo "CLOUDINARY_CLOUD_NAME=${CLOUDINARY_CLOUD_NAME}" >> .env
-echo "CLOUDINARY_URL=cloudinary://${CLOUDINARY_API_KEY}:${CLOUDINARY_API_SECRET}@${CLOUDINARY_CLOUD_NAME}" >> .env
-
 echo "[start.sh] 開始：環境変数を読み込みます..."
 source .env
 
