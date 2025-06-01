fc941a24178eee83a504c424e6d2c6a5abb26d0c
kirias1996 <arishou829@gmail.com>
2025-06-01 13:39:35 +0900

    バリデーションメッセージの呼び出し方法を変更

--- src/main/java/com/example/study/dto/UserCreateDto.java
+++ src/main/java/com/example/study/dto/UserCreateDto.java
@@ -2,16 +2,23 @@ package com.example.study.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserCreateDto {
-	@NotBlank
-	@Email
+	@NotBlank(message="{validation.email.blank}")
+	@Email(message="{validation.email.invalid}")
+	@Size(max=255,message="{validation.email.sizeover}")
 	private String email;
 	
-	@NotBlank
+	/*パスワードのハッシュ化にはBcryptアルゴリズムを採用
+	 * 72文字目以降は無視されてしまうため、72文字までの入力としている
+	 * */
+	@NotBlank(message="{validation.password.blank}")
+	@Size(min=8,max=72,message="{validation.password.size}")
 	private String password;
 	
-	@NotBlank
+	@NotBlank(message="{validation.password.blank}")
+	@Size(min=8,max=72,message="{validation.password.size}")
 	private String confirmPassword;
 	
 	public UserCreateDto() {
