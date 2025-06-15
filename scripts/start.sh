3f404b7a43c1b7a79b704cd9509d8b831fd0138a
kirias1996 <arishou829@gmail.com>
2025-06-15 16:42:05 +0900

    READMEの新規作成&セットアップ用資材の修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -e
 
-# echo "[start.sh] 環境変数TEST確認"
-# echo "[DEBUG] TEST (printenv) = $(printenv TEST)"
-# echo "[start.sh] 変数表示&ファイル出力"
-# echo "${TEST}"
-# echo "[start.sh] 変数表示&ファイル出力2"
-# echo "$TEST"
-# echo "$TEST" > test.txt
-# cat test.txt
-
 echo "[start.sh] 開始：環境変数を読み込みます..."
-# source .env
+source .env
 
 echo "[start.sh] Spring Boot アプリケーションを起動します..."
-java -jar build/libs/StudyLog.jar --spring.profiles.active=prod
+java -jar build/libs/StudyLog.jar --spring.profiles.active=local
 
-echo "[start.sh] .env を削除します（セキュリティ対策）..."
-rm .env
+# echo "[start.sh] .env を削除します（セキュリティ対策）..."
+# rm .env
